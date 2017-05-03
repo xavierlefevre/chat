ed01abdac320763ea8206fd741bd261de30fb898
Xavier Lefèvre <xavier.lefevre@me.com>
2017-05-03 22:26:39 +0200

    adding front end subscriptions

--- client/src/app.js
+++ client/src/app.js
@@ -4,6 +4,7 @@ import { ApolloProvider } from 'react-apollo';
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import ApolloClient, { createNetworkInterface } from 'apollo-client';
+import { SubscriptionClient, addGraphQLSubscriptions } from 'subscriptions-transport-ws';
 
 import { Routes, Scenes } from './routes';
 
@@ -13,8 +14,17 @@ global.FormData = global.originalFormData ? global.originalFormData : global.For
 const networkInterface = createNetworkInterface({
   uri: 'http://localhost:8080/graphql',
 });
+// Create WebSocket client
+const wsClient = new SubscriptionClient('ws://localhost:8080/subscriptions', {
+  reconnect: true,
+  connectionParams: {
+    // Pass any arguments you want for initialization
+  },
+});
+// Extend the network interface with the WebSocket
+const networkInterfaceWithSubscriptions = addGraphQLSubscriptions(networkInterface, wsClient);
 const client = new ApolloClient({
-  networkInterface,
+  networkInterface: networkInterfaceWithSubscriptions,
 });
 
 const store = createStore(
