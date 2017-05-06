5cdb19944a0582e519bb982c59022c3d183a1b98
Xavier Lefèvre <xavier.lefevre@me.com>
2017-05-06 12:21:46 +0200

    improve more file structure

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,2 +1,2 @@
 // @flow
-export { default as SelectedUserList } from './selected-user-list.component';
+export { SelectedUserList } from './selected-user-list';
---
+++ client/src/components/selected-user-list/index.js
@@ -0,0 +1,2 @@
+// @flow
+export { default as SelectedUserList } from './selected-user-list.component';
