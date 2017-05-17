1c9dc56bf0167654230cb2c27f541a6bc3621718
Xavier Lefèvre <xavier.lefevre@me.com>
2017-05-17 21:09:33 +0200

    rename snake case files

--- client/src/graphql/index.js
+++ client/src/graphql/index.js
@@ -1,13 +1,13 @@
 // @flow
 export { default as GROUP_QUERY } from './groups/group.query';
-export { default as CREATE_GROUP_MUTATION } from './groups/createGroup.mutation';
-export { default as LEAVE_GROUP_MUTATION } from './groups/leaveGroup.mutation';
-export { default as DELETE_GROUP_MUTATION } from './groups/deleteGroup.mutation';
-export { default as GROUP_ADDED_SUBSCRIPTION } from './groups/groupAdded.subscription';
+export { default as CREATE_GROUP_MUTATION } from './groups/create-group.mutation';
+export { default as LEAVE_GROUP_MUTATION } from './groups/leave-group.mutation';
+export { default as DELETE_GROUP_MUTATION } from './groups/delete-group.mutation';
+export { default as GROUP_ADDED_SUBSCRIPTION } from './groups/group-added.subscription';
 
 export { default as USER_QUERY } from './user/user.query';
 export { default as LOGIN_MUTATION } from './user/login.mutation';
 export { default as SIGNUP_MUTATION } from './user/signup.mutation';
 
-export { default as CREATE_MESSAGE_MUTATION } from './messages/createMessage.mutation';
-export { default as MESSAGE_ADDED_SUBSCRIPTION } from './messages/messageAdded.subscription';
+export { default as CREATE_MESSAGE_MUTATION } from './messages/create-message.mutation';
+export { default as MESSAGE_ADDED_SUBSCRIPTION } from './messages/message-added.subscription';
