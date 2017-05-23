31230724bf598c09a340b7e715c69fe1ab2b1576
Xavier Lefèvre <xavier.lefevre@me.com>
2017-05-23 13:51:58 +0200

    use friend fragment

--- client/src/graphql/user/add-friend.mutation.js
+++ client/src/graphql/user/add-friend.mutation.js
@@ -1,13 +1,15 @@
 // @flow
 import gql from 'graphql-tag';
 
+import FRIEND_FRAGMENT from '../fragments/friend.fragment';
+
 const ADD_FRIEND_MUTATION = gql`
   mutation addFriend($username: String!) {
     addFriend(username: $username) {
-      id
-      username
+      ... FriendFragment
     }
   }
+  ${FRIEND_FRAGMENT}
 `;
 
 export default ADD_FRIEND_MUTATION;
